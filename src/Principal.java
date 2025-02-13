e2a07f84328e5b466fdaec7fd269c5f858e0050c
Marilaine Medeiros <marilainemedeiros12@gmail.com>
2025-02-12 22:27:01 -0300

    exercicio1

---
+++ src/Principal.java
@@ -0,0 +1,13 @@
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Boas vindas aos Comandos de Repetição!");
+
+        int vezes = 20;
+        int i = 0;
+
+        while (i < vezes) {
+            System.out.println("Eu gosto de estudar Algoritmos!");
+            i++;
+        }
+    }
+}
